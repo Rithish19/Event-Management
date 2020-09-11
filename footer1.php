655c7d9bbdca241e9b4abcbbcac5dfafcded4c20
Rithish19 <70273728+Rithish19@users.noreply.github.com>
2020-09-11 14:32:55 +0530

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,59 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+    <section style="height:80px;"></section>
+    <!----------- Footer ------------>
+    <footer class="footer-bs">
+        <div class="row">
+        	<div class="col-md-3 footer-brand animated fadeInLeft">
+            	<h2>Logo</h2>
+                <p>Our library managment system project will make you to lend different books from different branches to read.</p>
+                <p>phone no:-81225551000</p>
+				<p> All rights reserved</p>
+            </div>
+        	<div class="col-md-4 footer-nav animated fadeInUp">
+            	<h4>Menu —</h4>
+            	<div class="col-md-6">
+                    <ul class="pages">
+                        <li><a href="admin.html">admin login</a></li>
+                        <li><a href="index.html">user login</a></li>
+                        <li><a href="register.html">register</a></li>
+                    </ul>
+                </div>
+            	<div class="col-md-6">
+                    <ul class="list">
+                        <li><a href="https://www.kluniversity.in">KLU Website</a></li>
+                        <li><a href="EmailForm.jsp">Contacts</a></li>
+                        <li><a href="gallery.jsp">Gallery</a></li>
+                    </ul>
+                </div>
+            </div>
+        	<div class="col-md-2 footer-social animated fadeInDown">
+            	<h4>Follow Us</h4>
+            	<ul>
+                	<li><a href="https://www.facebook.com/">Facebook</a></li>
+                	<li><a href="https://twitter.com/search?q=%23login&lang=en">Twitter</a></li>
+                	<li><a href="https://www.instagram.com/?hl=en">Instagram</a></li>
+                	<li><a href="https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession">Gmail</a></li>
+                </ul>
+            </div>
+        	<div class="col-md-3 footer-ns animated fadeInRight">
+            	<h4>Newsletter</h4>
+                <p>Any event you could search here</p>
+                <p>
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Search for...">
+                      <span class="input-group-btn">
+                        <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-envelope"></span></button>
+                      </span>
+                    </div><!-- /input-group -->
+                 </p>
+            </div>
+        </div>
+    </footer>
+    <section style="text-align:center; margin:10px auto;"><p>Designed by:-K.Rithish,K.Raghavendra,S.Noor,R.Sanath</a></p></section>
+
+</div>
